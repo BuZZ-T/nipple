3b542d0c4219d34b2587f84565ae6d267c061946
Bastian Gebhardt <Bastian.Gebhardt@jambit.com>
2018-01-05 22:58:52 +0100

    Simplified shortcut detection

--- nipple.py
+++ nipple.py
@@ -58,76 +58,27 @@ def on_press(key):
     global shift_pressed
     if key == keyboard.Key.ctrl:
         ctrl_pressed = True
-
-    if key == keyboard.Key.shift:
+    elif key == keyboard.Key.shift:
         shift_pressed = True
+    elif str(key)[:5] == 'Key.f':
+        if ctrl_pressed and shift_pressed:
+            play('nipple_shift_%s.ogg' % str(key)[4:])
+        elif ctrl_pressed:
+            play('nipple_%s.ogg' % str(key)[4:])
 
-    if ctrl_pressed and shift_pressed:
-        if key == keyboard.Key.f1:
-            play('nipple_s1.ogg')
-        elif key == keyboard.Key.f2:
-            play('nipple_s2.ogg')
-        elif key == keyboard.Key.f3:
-            play('nipple_s3.ogg')
-        elif key == keyboard.Key.f4:
-            play('nipple_s4.ogg')
-        elif key == keyboard.Key.f5:
-            play('nipple_s5.ogg')
-        elif key == keyboard.Key.f6:
-            play('nipple_s6.ogg')
-        elif key == keyboard.Key.f7:
-            play('nipple_s7.ogg')
-        elif key == keyboard.Key.f8:
-            play('nipple_s8.ogg')
-        elif key == keyboard.Key.f9:
-            play('nipple_s9.ogg')
-        elif key == keyboard.Key.f10:
-            play('nipple_s10.ogg')
-        elif key == keyboard.Key.f11:
-            play('nipple_s11.ogg')
-        elif key == keyboard.Key.f12:
-            play('nipple_s12.ogg')
-
-
-    elif ctrl_pressed:
-        if key == keyboard.Key.f1:
-            play('nipple1.ogg')
-        elif key == keyboard.Key.f2:
-            play('nipple2.ogg')
-        elif key == keyboard.Key.f3:
-            play('nipple3.ogg')
-        elif key == keyboard.Key.f4:
-            play('nipple4.ogg')
-        elif key == keyboard.Key.f5:
-            play('nipple5.ogg')
-        elif key == keyboard.Key.f6:
-            play('nipple6.ogg')
-        elif key == keyboard.Key.f7:
-            play('nipple7.ogg')
-        elif key == keyboard.Key.f8:
-            play('nipple8.ogg')
-        elif key == keyboard.Key.f9:
-            play('nipple9.ogg')
-        elif key == keyboard.Key.f10:
-            play('nipple10.ogg')
-        elif key == keyboard.Key.f11:
-            play('nipple11.ogg')
-        elif key == keyboard.Key.f12:
-            play('nipple12.ogg')
 
 def on_release(key):
     global ctrl_pressed
     global shift_pressed
     if key == keyboard.Key.ctrl:
         ctrl_pressed = False
-
-    if key == keyboard.Key.shift:
+    elif key == keyboard.Key.shift:
         shift_pressed = False
 
 def main():
     lis = keyboard.Listener(on_press=on_press, on_release=on_release)
-    lis.start() # start to listen on a separate thread
-    lis.join() # no this if main thread is polling self.keys
+    lis.start()
+    lis.join()
 
 if __name__ == '__main__':
     main()
